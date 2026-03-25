b286077fba6b4ab48778c24cd10be0b4966414ea
AnonymousRand <yhugdrive@gmail.com>
2026-03-25 16:14:13 -0700

    rename some tests

--- tests/thms/thm_counter/test_thm_counter.py
+++ tests/thms/thm_counter/test_thm_counter.py
@@ -8,22 +8,22 @@ from ...tests_utils import run_extension_test
     "extension, filename_base",
     [
         # docs example
-        (ThmsExtension(), "thms/thmcounter/success_1"),
-        (ThmsExtension(thm_counter_config={"add_html_elem": True}), "thms/thmcounter/success_2"),
+        (ThmsExtension(), "thms/thm_counter/success_1"),
+        (ThmsExtension(thm_counter_config={"add_html_elem": True}), "thms/thm_counter/success_2"),
         (
             ThmsExtension(thm_counter_config={"add_html_elem": True, "html_id_prefix": "foo"}),
-            "thms/thmcounter/success_3"
+            "thms/thm_counter/success_3"
         ),
         (
             ThmsExtension(thm_counter_config={"add_html_elem": True, "html_class": ":3"}),
-            "thms/thmcounter/success_4"
+            "thms/thm_counter/success_4"
         ),
         (
             ThmsExtension(thm_counter_config={"add_html_elem": True, "html_id_prefix": "alice", "html_class": "bob"}),
-            "thms/thmcounter/success_5"
+            "thms/thm_counter/success_5"
         ),
-        (ThmsExtension(), "thms/thmcounter/fail_1")
+        (ThmsExtension(), "thms/thm_counter/fail_1")
     ]
 )
-def test_thmcounter(extension, filename_base):
+def test_thm_counter(extension, filename_base):
     run_extension_test([extension], filename_base)
--- tests/thms/thm_heading/test_thm_heading.py
+++ tests/thms/thm_heading/test_thm_heading.py
@@ -7,22 +7,22 @@ from ...tests_utils import run_extension_test
 @pytest.mark.parametrize(
     "extension, filename_base",
     [
-        (ThmsExtension(), "thms/thmheading/success_1"),
-        (ThmsExtension(), "thms/thmheading/success_2"),
-        (ThmsExtension(), "thms/thmheading/success_3"),
-        (ThmsExtension(), "thms/thmheading/success_4"),
-        (ThmsExtension(), "thms/thmheading/success_5"),
+        (ThmsExtension(), "thms/thm_heading/success_1"),
+        (ThmsExtension(), "thms/thm_heading/success_2"),
+        (ThmsExtension(), "thms/thm_heading/success_3"),
+        (ThmsExtension(), "thms/thm_heading/success_4"),
+        (ThmsExtension(), "thms/thm_heading/success_5"),
         (
             ThmsExtension(thm_heading_config={
                 "html_id_prefix": "defenestrate-", "html_class": "bottom-text", "emph_html_class": "top-text"
             }),
-            "thms/thmheading/success_6"
+            "thms/thm_heading/success_6"
         ),
 
         # test that curly braces (e.g. from LaTeX) don't interfere with parsing
-        (ThmsExtension(), "thms/thmheading/success_7"),
-        (ThmsExtension(), "thms/thmheading/fail_1")
+        (ThmsExtension(), "thms/thm_heading/success_7"),
+        (ThmsExtension(), "thms/thm_heading/fail_1")
     ]
 )
-def test_thmheading(extension, filename_base):
+def test_thm_heading(extension, filename_base):
     run_extension_test([extension], filename_base)
